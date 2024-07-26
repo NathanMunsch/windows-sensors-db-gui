c23e3e9d7d89934b657d5f71771425576789a845
NathanMunsch <nathan2003munsch@gmail.com>
2024-07-26 19:57:41 +0200

    feat: Create computer store

---
+++ src/store/computerStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+import axios from 'axios';
+
+type ComputerStore = {
+    computers: [
+        {
+            Id: number,
+            Name: string,
+        }
+    ];
+    fetchComputers: () => void;
+};
+
+export const computerStore = create<ComputerStore>((set) => ({
+    computers: [
+        {
+            Id: 0,
+            Name: '',
+        }
+    ],
+
+    fetchComputers: () => {
+        axios.get('/api/computers')
+            .then((response) => {
+                // @ts-ignore
+                computerStore.getState().computers.length = 0;
+
+                response.data.forEach((computer: any) => {
+                    computerStore.getState().computers.push({
+                        Id: computer.Id,
+                        Name: computer.Name,
+                    });
+                }, []);
+            })
+            .catch((error) => {
+                console.error('Error fetching computers:', error);
+            });
+    },
+}));
