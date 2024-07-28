038cc1f2aa520d076857e3fd6103060e2e151d85
NathanMunsch <nathan2003munsch@gmail.com>
2024-07-28 03:15:04 +0200

    feat: Update home page to handle pinning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,26 @@
 import Header from "@/components/Header";
 import StatCard from "@/components/StatCard";
 import {statsStore} from "@/store/statsStore";
+import {pinnedStore} from "@/store/pinnedStore";
+import Separator from "@/components/Separator";
 
 export default function Home() {
-
     const {
         stats
     } = statsStore();
+
+    const {
+        isStatPinned
+    } = pinnedStore();
 
     return (
         <main>
             <Header />
             <br />
+            <Separator text={"Pinned"} />
             <section className="grid grid-cols-[repeat(auto-fit,_16.666666%)] m-auto justify-center gap-2">
                 {stats.map((stat, index) => (
+                    isStatPinned(stat.SensorName) &&
                     <StatCard key={index}
                               sensorName={stat.SensorName}
                               value={stat.MeasuredValue}
@@ -25,6 +32,20 @@ export default function Home() {
                               unit={stat.Unit}
                     />
                 ))}
+            </section>
+            <Separator text={"All"} />
+            <section className="grid grid-cols-[repeat(auto-fit,_16.666666%)] m-auto justify-center gap-2">
+                {stats.map((stat, index) => (
+                        <StatCard key={index}
+                                  sensorName={stat.SensorName}
+                                  value={stat.MeasuredValue}
+                                  highestValueAllTime={stat.HighestValueAllTime}
+                                  lowestValueAllTime={stat.LowestValueAllTime}
+                                  averageValueAllTime={stat.AverageValueAllTime}
+                                  unit={stat.Unit}
+                        />
+                    )
+                )}
             </section>
         </main>
     );
