226e748c23eec312acf64e878c0951ee47d14e85
NathanMunsch <nathan2003munsch@gmail.com>
2024-07-28 03:14:45 +0200

    feat: Create pinnedStore.ts to handle pinning

---
+++ src/store/pinnedStore.ts
@@ -0,0 +1,50 @@
+import {create} from 'zustand';
+import {persist} from 'zustand/middleware';
+
+type PinnedStore = {
+    statPinned: string[];
+    getStatPinned: () => string[];
+    addStatPinned: (stat: string) => void;
+    removeStatPinned: (stat: string) => void;
+    toggleStatPinned: (stat: string) => void;
+    isStatPinned: (stat: string) => boolean;
+};
+
+export const pinnedStore = create<PinnedStore>()(
+    persist(
+        (set, get) => ({
+            statPinned: [],
+            getStatPinned: () => {
+                return get().statPinned;
+            },
+            addStatPinned: (stat: string) => {
+                if (!get().statPinned.includes(stat)) {
+                    set((state) => ({
+                        statPinned: [...state.statPinned, stat],
+                    }));
+                }
+            },
+            removeStatPinned: (stat: string) => {
+                set((state) => ({
+                    statPinned: state.statPinned.filter((pinedStat) => pinedStat !== stat),
+                }));
+            },
+            toggleStatPinned: (stat: string) => {
+                if (get().statPinned.includes(stat)) {
+                    get().removeStatPinned(stat);
+                } else {
+                    get().addStatPinned(stat);
+                }
+            },
+            clearStatPinned: () => {
+                set({statPinned: []});
+            },
+            isStatPinned: (stat: string) => {
+                return get().statPinned.includes(stat);
+            },
+        }),
+        {
+            name: 'pinned-store',
+        }
+    )
+);
