885deca6ea42b5046032ef88c43e8c2d1b2573c2
NathanMunsch <nathan2003munsch@gmail.com>
2024-07-31 21:10:56 +0200

    refactor: Remove unused parameter

--- src/app/api/date-measurements/latest/route.ts
+++ src/app/api/date-measurements/latest/route.ts
@@ -2,7 +2,7 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
+export async function GET() {
     const latestDateMeasurement = await prisma.dateMeasurementEntities.findFirstOrThrow({
         orderBy: {
             Date: 'desc'
